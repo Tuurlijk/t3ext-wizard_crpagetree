4f5a3b6638bcae36a422dbbfc3448f6cbc21290f
Michiel Roos <michiel@michielroos.com>
2019-05-08 09:21:56 +0200

    Cleanup javaScript

--- Resources/Public/JavaScript/ContextMenuActions.js
+++ Resources/Public/JavaScript/ContextMenuActions.js
@@ -1,13 +1,9 @@
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-define(["require", "jquery", "TYPO3/CMS/Backend/Viewport"], function (e, r, s) {"use strict";return function () {return e.pagesNewTree = function () {var n = r(this).data("page-new-tree-url");n && s.ContentContainer.setUrl(n)}, e}()});
+define(["require", "jquery", "TYPO3/CMS/Backend/Viewport"], function (require, jQuery, backendViewport) {
+    "use strict";
+    return function () {
+        return require.pagesNewTree = function () {
+            var url = jQuery(this).data("page-new-tree-url");
+            url && backendViewport.ContentContainer.setUrl(url);
+        }, require
+    }()
+});
